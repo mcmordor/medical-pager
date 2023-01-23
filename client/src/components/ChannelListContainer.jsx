cd98743ef4ccf15b2e592b64fe0c74f29cc32b89
Isaak Meakin <ismeakin@gmail.com>
2023-01-23 14:07:49 -0700

    Comment ToDo ChannelListContainer
    Create reminder for Channel List Container imports Channel Search

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+//TODO: import the comented out shit left off at youtube timestamp 25:20 https://www.youtube.com/watch?v=MJzbJQLGehs&t=1047s
 //import { ChannelList, useChatContext} from 'stream-chat-react';
 //import Cookies from 'universal-cookie';
 //import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';

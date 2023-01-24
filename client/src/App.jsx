7fa71e31dbae542ce6ffa04a1c5c72813611280c
Isaak Meakin <ismeakin@gmail.com>
2023-01-24 15:59:33 -0700

    Add TeamChannelList TeamChannel Preview and Auth
    Add Team Channel List, Team Channel Preview, Auth and axios install for signup page. Also change assets to .jsx instead of .js.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import {ChannelListContainer, ChannelContainer } from './components';
+import {ChannelListContainer, ChannelContainer, Auth } from './components';
 import './App.css';
 
 const apiKey = 'zman9kwqm2w6';
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 //the actual app thing
 const App = () => {
+  if(!authToken) return <Auth />
+  
   return (
     <div className="app__wrapper">
         <Chat client={client} theme="team light">

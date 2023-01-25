084af23539908d3a68550549b090c6dd15c9e263
Isaak Meakin <ismeakin@gmail.com>
2023-01-25 16:38:30 -0700

    backend setup
    signup signin server setup

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT ||5000;
+
+const authRoutes = require('./routes/auth.js');
+
+require ('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req,res) =>{
+    res.send('hello world!');
+
+});
+
+app.use('/auth', authRoutes);
+
+app.listen(PORT,() => console.log(`Server runnon on port ${PORT}`));

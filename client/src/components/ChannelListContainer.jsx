988559369e7195d44ca5007538f858635662d77f
Isaak Meakin <ismeakin@gmail.com>
2023-01-26 13:33:13 -0700

    Fix Logout button
    fix logout button TeamChannelPreview declared setIsCreating.

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
-import { ChannelList, useChatContext} from 'stream-chat-react';
+import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import { ChannelSearch, TeamChannelList, TeamChannelPreview } from '../components'; 
-import HospitalIcon from '../assets/hospital.png';
-import LogoutIcon from '../assets/logout.png';
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+import HospitalIcon from '../assets/hospital.png'
+import LogoutIcon from '../assets/logout.png'
+
 const cookies = new Cookies();
 
 
 const SideBar = ({ logout }) => (
   <div className="channel-list__sidebar">
       <div className="channel-list__sidebar__icon1">
-        <div className="icon1__inner">
-          <img src ={HospitalIcon} alt="Hopspital" width="30" />
-        </div>
+          <div className="icon1__inner">
+              <img src={HospitalIcon} alt="Hospital" width="30" />
+          </div>
       </div>
       <div className="channel-list__sidebar__icon2">
-        <div className="icon1__inner">
-          <img src ={LogoutIcon} alt="Logout" width="30" />
-        </div>
+          <div className="icon1__inner" onClick={logout}>
+              <img src={LogoutIcon} alt="Logout" width="30" />
+          </div>
       </div>
   </div>
 );

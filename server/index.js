71132c0ac8749399a5c8dedca04fe743a72fd9d5
Isaak Meakin <ismeakin@gmail.com>
2023-01-25 17:27:11 -0700

    Fix Sign Up Server issue
    change https to http local host can't to https. can create user now.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/auth.js');
 
 const app = express();
 const PORT = process.env.PORT ||5000;
-
-const authRoutes = require('./routes/auth.js');
 
 require ('dotenv').config();
 
@@ -19,4 +18,4 @@ app.get('/', (req,res) =>{
 
 app.use('/auth', authRoutes);
 
-app.listen(PORT,() => console.log(`Server runnon on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
